b2a4cdd6604091684cf34d17c7f6ec3d7b650ae9
Sanika Kalvikatte <88078801+SanikaMK@users.noreply.github.com>
2024-01-03 12:18:33 -0800

    Add files via upload

---
+++ final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        X = 0
+        for op in operations:
+            if op in ("++X", "X++"):
+                X+=1
+            elif op in ("--X", "X--"):
+                X-=1
+        return X

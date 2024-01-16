9e23bc728a01513ea6d399b4bb876514b95e57e7
Sanika Kalvikatte <88078801+SanikaMK@users.noreply.github.com>
2024-01-15 19:06:49 -0500

    Create Faulty Keyboard.py

---
+++ Faulty Keyboard.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def finalString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        result  = []
+        for char in s:
+            if char == 'i':
+                result.reverse()
+            else:
+                result.append(char)
+        return ''.join(result)
+        
